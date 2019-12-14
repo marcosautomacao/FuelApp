55b1a689ad97d996d4c57ce6ac5d9360d42bd110
Logon Aluno <logonaluno@local.com>
2019-12-14 11:15:11 -0300

    Configuracao inicial e telas

---
+++ app/src/main/java/br/com/marcos/calculadoraflex/ui/form/FormActivity.kt
@@ -0,0 +1,13 @@
+package br.com.marcos.calculadoraflex.ui.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.marcos.calculadoraflex.R
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
---
+++ app/src/main/java/br/com/marcos/calculadoraflex/ui/signup/SignupActivity.kt
@@ -0,0 +1,11 @@
+package br.com.marcos.calculadoraflex.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
